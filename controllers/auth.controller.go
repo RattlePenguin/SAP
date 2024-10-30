1d9e6e11850aec233f97d896af4c1da2b7926072
Benedict <z5457257@ad.unsw.edu.au>
2024-10-31 03:43:44 +1100

    Added login route, TODO: test logins

--- controllers/auth.controller.go
+++ controllers/auth.controller.go
@@ -35,6 +35,7 @@ func (ac *AuthController) RegisterUser(ctx *gin.Context) {
 		Password:	payload.Password,
 	}
 
+	// SQL Injection Site
 	result := ac.DB.Create(&newUser)
 
 	// Error Handling
@@ -49,4 +50,30 @@ func (ac *AuthController) RegisterUser(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusCreated, gin.H{"status": "success", "message": "Registered successfully!"})
+}
+
+// Logs in a user
+func (ac *AuthController) LoginUser(ctx *gin.Context) {
+	var payload *models.LoginUserInput
+
+	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	// SQL Injection Site
+	var user models.User
+	result := ac.DB.First(&user, "email = ?", strings.ToLower(payload.Email))
+	if result.Error != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Invalid Email or Password"})
+		return
+	}
+
+	userResponse := gin.H{
+		"id":          user.ID.String(),
+		"name":        user.Name,
+		"email":       user.Email,
+		"otp_enabled": user.Otp_enabled,
+	}
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "user": userResponse})
 }
--- test.sh
+++ test.sh
@@ -4,8 +4,25 @@
 curl "http://localhost:8000/api/healthchecker"
 echo ""
 
-# Check that user can be successfully registered
+# Check that new user can be successfully registered
+curl -d \
+    "{\"Name\":\"John Doe 1\",\"Email\":\"john.doe@email.com\",\"Password\":\"johndoe123\"}" \
+    -H "Content-Type: application/json" http://localhost:8000/api/auth/register
+echo ""
+
+# Check that same email address cannot be used
 curl -d \
     "{\"Name\":\"John Doe 2\",\"Email\":\"john.doe@email.com\",\"Password\":\"johndoe123\"}" \
     -H "Content-Type: application/json" http://localhost:8000/api/auth/register
 echo ""
+
+# Check that user can be logged in
+
+# Verify cookie given to already logged in user
+
+
+# SQL Vulnerability 1: SELECT user with no 2FA enabled
+# SQL Vulnerability 2: SELECT specific user with no 2FA enabled
+# SQL Vulnerability 3: Disable user's Otp_enabled value
+
+# XSS Vulnerability 1: Stealing Cookies
